79b4f42b8b3b8218671401df3f9080faafc1bc26
Mila Paymukhina <milanyaxo@gmail.com>
2021-03-01 00:46:43 -0500

    V2

---
+++ flixapp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixapp
+//
+//  Created by Mila Paymukhina on 26.02.2021.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
